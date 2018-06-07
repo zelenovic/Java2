4bd01357e73d20e4598b49c34c8fa0a01b7a28ca
AnaRadojevic <anaradojevic90@gmail.com>
2018-06-07 19:56:10 +0200

    Drugi commit za vezbu 2, napravljena klasa Course

---
+++ src/course/Course.java
@@ -0,0 +1,28 @@
+
+package course;
+
+
+public class Course {
+    
+    private String name;
+    private int numberOfClasses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+    
+    
+    
+}
