f67361842c48c52af7b5736cdff4089935141c3b
AnaRadojevic <anaradojevic90@gmail.com>
2018-06-07 20:00:29 +0200

    Treci commit za vezbu 2, dodata klasa Computer

---
+++ src/computer/Computer.java
@@ -0,0 +1,37 @@
+
+package computer;
+
+
+public class Computer {
+    
+    private double processTact;
+    private double memory;
+    private int hardDrive;
+
+    public double getProcessTact() {
+        return processTact;
+    }
+
+    public void setProcessTact(double processTact) {
+        this.processTact = processTact;
+    }
+
+    public double getMemory() {
+        return memory;
+    }
+
+    public void setMemory(double memory) {
+        this.memory = memory;
+    }
+
+    public int getHardDrive() {
+        return hardDrive;
+    }
+
+    public void setHardDrive(int hardDrive) {
+        this.hardDrive = hardDrive;
+    }
+    
+    
+    
+}
