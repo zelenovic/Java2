59e5489803d10f49cd0bd5aca5221faa48e7cca6
AnaRadojevic <anaradojevic90@gmail.com>
2018-06-07 20:31:49 +0200

    Sedmi commit, napravljen objekat klase Student, dodeljeni i ispisani podaci

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import student.Student;
+
 public class Main {
     
     public static void main(String[] args) {
         
+        Student s1 = new Student();
+        s1.setFirstName("Milan");
+        s1.setLastName("Zelenovic");
+        s1.setYearOfBirth(2000);
         
+        s1.getCourse().setName("Java course");
+        s1.getCourse().setNumberOfClasses(55);
+        s1.getComputer().setProcessTact(3);
+        s1.getComputer().setMemory(12);
+        s1.getComputer().setHardDrive(768);
+        
+        s1.info();
         
     }
     
--- src/student/Student.java
+++ src/student/Student.java
@@ -59,7 +59,7 @@ public class Student {
         System.out.println("Last name: " + lastName);
         System.out.println("Year of birth: " + yearOfBirth);
         System.out.println("Course name: " + course.getName());
-        System.out.println("Course number of classes:" + course.getNumberOfClasses());
+        System.out.println("Course number of classes: " + course.getNumberOfClasses());
         System.out.println("Process tact: " + computer.getProcessTact());
         System.out.println("Memory: " + computer.getMemory());
         System.out.println("Hard driver: " + computer.getHardDrive());
